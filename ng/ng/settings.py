5a2395631609ce2b94f755389daa01775964f21f
Owen Nelson <onelson@gmail.com>
2013-12-31 00:25:55 -0800

    setup urls for ng app static files and main view

--- ng/ng/settings.py
+++ ng/ng/settings.py
@@ -80,3 +80,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+TEMPLATE_DIRS = (
+    # for production, point at client-src/dist/ instead of client-src/app/
+    os.path.join(BASE_DIR, 'ng', 'client-src', 'app'),
+)
--- ng/ng/urls.py
+++ ng/ng/urls.py
@@ -1,6 +1,11 @@
+import os
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +14,31 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name='index.html'))
 )
+
+
+if settings.DEBUG:
+    # Note that any dir names added here may need to have some characters
+    # escaped since they are getting dropped into a regex.
+    __ng_app_static_dirs = (
+        'bower_components',
+        'images',
+        'scripts',
+        'styles',
+        'views'
+    )
+
+    urlpatterns += patterns(
+        '',
+        url(r'^(?P<path>((favicon\.ico|robots\.txt)|'
+            r'(%s)/.*))$' % '|'.join(__ng_app_static_dirs),
+            'django.views.static.serve',
+            {'document_root': os.path.join(
+                settings.BASE_DIR,
+                'ng',
+                'client-src',
+                'app', )})
+    )
+
+    urlpatterns += staticfiles_urlpatterns()
